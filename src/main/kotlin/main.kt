b25afb391a83a0faf6621e8b7daa9c52289745b7
Irina Makarova <irina.makarova@bk.ru>
2024-02-25 17:03:29 +0300

    changing the commission amount

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     val amount = 1000
 
-    val amountCommission = amount * 0.75
+    val amountCommission = amount * 0.0075
     val result = if (amountCommission > 35) amountCommission else 35
     print("размер комиссии $result руб.")
 }
